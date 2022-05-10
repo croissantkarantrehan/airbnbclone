04ce4ef0e196b2411e78ee8ab5da1a90f0d26fee
croissantkarantrehan <104403970+croissantkarantrehan@users.noreply.github.com>
2022-05-10 09:58:12 +0530

    fixing second

--- src/component/Second.js
+++ src/component/Second.js
@@ -2,7 +2,7 @@ import React from 'react'
  export default function  Second()
 {
     return(<section className='second'>
-        <img src='../image/gridphoto.webp' alt="grid" className="grid"></img>   
+        <img src='./image/gridphoto.webp' alt="grid" className="grid"></img>   
         <h1 className="second-head">Online Experiences</h1>
         <p className="second-para">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
 </p>
